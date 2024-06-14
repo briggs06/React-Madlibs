fd2ee47664f2a6ba59789c5effe2f0438206f757
briggs06 <148621947+briggs06@users.noreply.github.com>
2024-06-13 20:14:46 -0400

    Add files via upload

---
+++ react-1/madlib/src/textinput.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useField } from 'formik';
+import styled from '@emotion/styled';
+
+const TextInput = ({ label, ...props }) => {
+  const StyledErrorMessage = styled.div`
+    font-size: 12px;
+    color: var(--red-600);
+    width: 400px;
+    margin-top: 0.25rem;
+    &:before {
+      content: '❌ ';
+      font-size: 10px;
+    }
+    @media (prefers-color-scheme: dark) {
+      color: var(--red-300);
+    }
+  `;
+  const [field, meta] = useField(props);
+  return (
+    <>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <input className="text-input" {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <StyledErrorMessage>{meta.error}</StyledErrorMessage>
+      ) : null}
+    </>
+  );
+};
+
+export default TextInput;
